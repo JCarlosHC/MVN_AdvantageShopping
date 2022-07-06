eb0c0bcd26d4b89357b2388e84360dc9ad38a6b8
Juan Carlos Hernandez <juanhernandezcastro.c@gmail.com>
2022-07-06 17:55:13 -0500

    Added getExcelData function

--- src/test/java/advantageShopping/support/ExcelPropertyLoader.java
+++ src/test/java/advantageShopping/support/ExcelPropertyLoader.java
@@ -4,9 +4,12 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.util.Iterator;
 import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.util.Dictionary;
 import java.util.Hashtable;
@@ -19,7 +22,7 @@ public class ExcelPropertyLoader {
 			FileInputStream inputStream = new FileInputStream(new File(rutaArchivoExcel));
 			Workbook workbook = new XSSFWorkbook(inputStream);
 			Sheet firstSheet = workbook.getSheetAt(0);
-			Iterator iterator = firstSheet.iterator();
+			Iterator<Row> iterator = firstSheet.iterator();
 			DataFormatter formatter = new DataFormatter();
 
 			dic = new Hashtable<String, String>();
@@ -35,6 +38,33 @@ public class ExcelPropertyLoader {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	public String[][] getExcelData(String fileName, String sheetName, int numberOfColumns) {
+
+		String[][] data = null;
+		try {
+			DataFormatter formatter = new DataFormatter();
+			FileInputStream fis = new FileInputStream(fileName);
+			XSSFWorkbook wb = new XSSFWorkbook(fis);
+			XSSFSheet sh = wb.getSheet(sheetName);
+			XSSFRow row = sh.getRow(0);
+			int noOfRows = sh.getLastRowNum() - 1;
+			Cell cell;
+			data = new String[noOfRows - 1][numberOfColumns];
+			for (int i = 1; i < noOfRows; i++) {
+				for (int j = 0; j < numberOfColumns; j++) {
+					row = sh.getRow(i);
+					cell = row.getCell(j);
+					data[i - 1][j] = formatter.formatCellValue(cell);
+				}
+			}
+			wb.close();
+			fis.close();
+		} catch (Exception e) {
+			System.out.println("The exception is: " + e.getMessage());
+		}
+		return data;
 	}
 
 	public String getValue(String key) {
